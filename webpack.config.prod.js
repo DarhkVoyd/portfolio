1b7fa03d8ed27736335a44d25d14640700561b31
D V <divyadityasnaruka@gmail.com>
2023-10-21 18:52:46 +0530

    update webpack to copy public

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,7 +24,7 @@ module.exports = {
 	},
 	plugins: [
 		new CopyWebpackPlugin({
-			patterns: ['./public/index.html', './public/styles.css'],
+			patterns: [{ from: './public', to: './' }],
 		}),
 	],
 };
