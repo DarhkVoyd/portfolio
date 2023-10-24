2efbdfe91aeb6bf3fe33878f8eea71b69a461a83
D V <divyadityasnaruka@gmail.com>
2023-10-24 21:33:31 +0530

    refactor

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,18 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#root')});
-
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-camera.position.z = 10;
-
 const geometry = new THREE.TorusGeometry(10, 3, 16, 50);
 const material = new THREE.MeshBasicMaterial( { color: 0x777777 , wireframe: true} );
 const torus = new THREE.Mesh( geometry, material );
 scene.add( torus );
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 10;
+
+const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#webgl')});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
 
 const animate = () => {
     requestAnimationFrame(animate);
