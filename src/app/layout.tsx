ecb556b31f073658fbcae10cf1f1e1ab3452149a
D V <divyadityasnaruka@gmail.com>
2024-02-03 11:43:02 +0530

    add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import "./globals.css";
 
@@ -15,6 +16,7 @@ export default function RootLayout({
         <html lang="en">
             <body>
                 {children}
+                <SpeedInsights />
             </body>
         </html>
     );
