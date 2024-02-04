2fda09b4cc9876c19b5ce240eb823cb568eefb66
D V <divyadityasnaruka@gmail.com>
2024-02-04 12:40:39 +0530

    migrate to tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,26 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-    title: "JIGSAW",
-    description: "Portfolio",
+	title: 'JIGSAW',
+	description: 'Portfolio',
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body>
-                {children}
-                <SpeedInsights />
-            </body>
-        </html>
-    );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				{children}
+				<SpeedInsights />
+			</body>
+		</html>
+	);
 }
